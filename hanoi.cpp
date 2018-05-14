6020d905de3f32a82582d3663629f4dee0ee2ea7
cyqiang <chenyongqiang@tsinghua.edu.cn>
2018-05-14 17:17:11 +0800

    branch1
    mynewbranch1

---
+++ hanoi.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void move(char a,char b)
+{
+	cout<<a<<"->"<<b<<endl;
+}
+void  hanoi(int n, char a, char b, char c)
+{
+	if(n==1)
+	{
+		move(a,c);
+	}
+	else
+	{
+		hanoi(n-1,a,c,b);
+		move(a,c);
+		hanoi(n-1,b,a,c);
+	}
+
+}
+int main()
+{
+	char a='A',b='B',c='C';
+	hanoi(3,a,b,c);
+    cout<<"hello"<<endl;
+}
